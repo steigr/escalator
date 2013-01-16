ee5a4a05cd9d43265954021a799ae33b7c8890cd
Mathias Kaufmann <me@stei.gr>
2013-01-16 09:47:19 +0100

    Added basic bootstrap-code to escalator-frontend

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def twitterized_type(type)
+    case type
+      when :alert
+        "alert-block"
+      when :error
+        "alert-error"
+      when :notice
+        "alert-info"
+      when :success
+        "alert-success"
+      else
+        type.to_s
+    end
+  end
+  def active_navigation(given_controller)
+    "active" if current_page?(url_for(:controller => given_controller))
+  end
 end
