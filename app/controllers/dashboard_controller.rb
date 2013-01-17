a2575521b3f2fea63f4d77080c90befbaba0eeac
Mathias Kaufmann <me@stei.gr>
2013-01-17 09:07:01 +0100

    Added some counters for dashboard and split dashboard in multiple parts
    Signed-off-by: Mathias Kaufmann <me@stei.gr>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,11 @@
 class DashboardController < ApplicationController
+	def index
+    	@issuecount = Issue.all.count
+    	@usercount = User.all.count
+		@rotationcount = Rotation.all.count    	
+
+    	respond_to do |format|
+			format.html
+		end
+	end
 end
